b562ecd3a1b31c609282beb3ac091776251f571a
dawidspamer <dawidspamer@gmail.com>
2014-04-20 00:05:37 +0000

    removing borders and centering main image

--- slide.js
+++ slide.js
@@ -1,40 +1,49 @@
-//test //alert("Hello")
-//starting with document.ready func
-$(function(){
+// -------
+  // #Instagram Images
+// -------
 
-  $( ".thumb-link" ).click(function() {
-  //alert( "Image is clickable" );
+$(function () {
+
+  $( 'body' ).on('click','.thumb-link', function() {
     var src = $(this).find('img').attr('src');
-    //console.log(src);
-    //console.log(typeof(src));
-
-    src = src.replace("thumb","main");
-    //src.replace('thumb','main');
-
-    $('#main-image').attr('src',src); 
+    $('#main-image').attr('src', src);
+    
+    $('#image-display').show('slow');
+    
+    $( 'body' ).on('click','#image-display',function() {
+      $( "#image-display" ).hide( 1000 );
+    }); 
   });
 
-  $(".thumb-link").click(function(){
-      var figcap = $(this).find("h5").text();
-      $("#main-img-title").find("h5").text(figcap);
+  
+  connectAPI();
+  
+  setInterval(function() {
+    connectAPI();
+  }, 300000);
 
-  });
+  function connectAPI() {
+    var url = "https://api.instagram.com/v1/tags/lighthouse/media/recent?client_id=a17b202a68d644619d0f4b7d0c4c498b&callback=?";
+    options = {
+      dataType: 'jsonp',
+      success: loadImages,  
+      error: function() {
+        alert('error');
+      }
+    };
 
-  $(".thumb-link").hover(function(){
-    $(".thumb-link").css("border-color", "yellow")
+    $.ajax(url, options);
+  }
 
-  });
-
-  $("#thumb-container li").hover(function(){
-    $(this).find('img').css("border-color" ,"red").css("opacity" ,"0.3");
-  },function(){
-    $(this).find('img').css("border-color","rgba(131, 130, 130, 0.21)").css("opacity","1");
-  });
-
-  $("#thumb-container li").hover( function() {
-    $(this).find('.img-title').fadeIn(300);
-  }, function() {
-    $(this).find('.img-title').fadeOut(100);
-  });
-
+  function loadImages(data) {
+    $('.clearfix').empty();
+    // console.log(data.data.length);
+    for(var i=0; i<data.data.length; i++){
+      var post = data.data[i]
+      var lowResUrl = post.images.low_resolution.url
+      $('<li class="thumb-link"><a href="#main-image"><img src="'+lowResUrl+'"/></a></li>').appendTo($('.clearfix'));
+    
+    }
+  }
 });
+
